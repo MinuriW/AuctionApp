edaaca4a2be1ab40565fc61e5b9947f8fe9fa9d5
sudarshanapanditha <sudarshana.scholar@gmail.com>
2021-01-23 15:47:10 +0530

    Add AuctionItemDAOImpl class

---
+++ src/com/hcl/seller/dao/AuctionItemDAOImpl.java
@@ -0,0 +1,15 @@
+package com.hcl.seller.dao;
+
+import java.io.InputStream;
+import java.util.Date;
+
+public class AuctionItemDAOImpl implements AuctionItemDAO {
+
+	@Override
+	public Boolean insertAuctionItem(String title, String condition, Date startDate, Double startingPrice,
+			InputStream photoStream) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
