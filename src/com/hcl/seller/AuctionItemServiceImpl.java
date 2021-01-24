3a319868eeba8459ab7db087ac6fce0aa89fb8d9
sudarshanapanditha <sudarshana.scholar@gmail.com>
2021-01-25 00:51:59 +0530

    Change AuctionItemServiceImpl class

--- src/com/hcl/seller/AuctionItemServiceImpl.java
+++ src/com/hcl/seller/AuctionItemServiceImpl.java
@@ -1,22 +1,43 @@
 package com.hcl.seller;
 
 import java.io.InputStream;
+import java.sql.Timestamp;
+import java.util.Date;
+
+import com.hcl.seller.dao.AuctionItemDAO;
+import com.hcl.seller.dao.AuctionItemDAOImpl;
+import com.hcl.seller.util.PhotoManager;
+import com.hcl.seller.util.PhotoManagerImpl;
 
 public class AuctionItemServiceImpl implements AuctionItemService {
+	private AuctionItemDAO auctionItemDAO;
+	private PhotoManager photoManager;
 
-	@Override
-	public Boolean submitAuctionItem(
-			Integer userId, 
-			String title, 
-			String condition, 
-			Integer timePeriod,
-			Double startingPrice, 
-			InputStream photoStream) {
-		
-		return null;
-		
+	public AuctionItemServiceImpl(String resourcePath) {
+		super();
+		auctionItemDAO = new AuctionItemDAOImpl();
+		photoManager = new PhotoManagerImpl(resourcePath);
 	}
 
-	
+	@Override
+	public Boolean submitAuctionItem(String title, String condition, Integer timePeriod,
+			Double startingPrice, InputStream photo) {
+
+		Date auctionStart = new Date();
+		long milliseconds = auctionStart.getTime();
+
+		milliseconds += (timePeriod * 24 * 60 * 60 * 1000);
+		Date auctionEnd = new Date(milliseconds);
+
+		Timestamp startDate = new Timestamp(auctionStart.getTime());
+		Timestamp endDate = new Timestamp(auctionEnd.getTime());
+		
+		String photoURL = photoManager.savePhoto(photo);
+
+		System.out.println(photoURL);
+		
+		return auctionItemDAO.insertAuctionItem(title, condition, startDate, endDate, startingPrice, photoURL);
+
+	}
 
 }
