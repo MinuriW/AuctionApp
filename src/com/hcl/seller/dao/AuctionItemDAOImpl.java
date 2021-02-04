9bc4896359cbeab7ac998216b030c579e591d2ea
sudarshanapanditha <sudarshana.scholar@gmail.com>
2021-02-05 00:43:29 +0530

    Implement getAuctionItemById method

--- src/com/hcl/seller/dao/AuctionItemDAOImpl.java
+++ src/com/hcl/seller/dao/AuctionItemDAOImpl.java
@@ -1,6 +1,10 @@
 package com.hcl.seller.dao;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
 import com.hcl.seller.domain.AuctionItem;
+import com.hcl.util.HibernateUtil;
 
 public class AuctionItemDAOImpl implements AuctionItemDAO {
 
@@ -12,7 +16,16 @@ public class AuctionItemDAOImpl implements AuctionItemDAO {
 
 	@Override
 	public AuctionItem getAuctionItemById(int id) {
-		return null;
+		SessionFactory sessionFactory = HibernateUtil.geSessionFactory();
+		
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		
+		AuctionItem auctionItem = (AuctionItem) session.get(AuctionItem.class, new Integer(id));
+		
+		session.getTransaction().commit();
+		
+		return auctionItem;
 	}
 
 	
