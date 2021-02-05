fcb472f30fc5a69b9e86c2df7f2d9ce8d069ec89
sudarshanapanditha <sudarshana.scholar@gmail.com>
2021-02-05 20:09:48 +0530

    Modify UserDAO interface and UserDAOImpl class
    Add getUserByUsername method to UserDAO interface and modify
    UserDAOImpl to implement getUserByUsername method

--- src/com/hcl/user/dao/UserDAOImpl.java
+++ src/com/hcl/user/dao/UserDAOImpl.java
@@ -19,15 +19,8 @@ import com.hcl.util.HibernateUtil;
 
 public class UserDAOImpl implements UserDAO {
 
-	//private static final String SELECT_USER_BY_EMAIL = "SELECT * FROM auction_item WHERE email=?";
-	//private static final String INSERT_USER = "INSERT INTO \"user\" VALUES (user_id_seq.NEXTVAL, ?, ?, ?, ?)";
-	
-	//SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-	private DbConnection dbConnection;
-
 	public UserDAOImpl() {
 		super();
-		dbConnection = new DbConnectionImpl();
 	}
 
 	@Override
@@ -63,6 +56,12 @@ public class UserDAOImpl implements UserDAO {
 
 		session.getTransaction().commit();
 		return isSaved;
+	}
+
+	@Override
+	public User getUserByUsername(String username) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
