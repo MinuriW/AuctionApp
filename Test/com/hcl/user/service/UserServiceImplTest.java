eeeaa42fca9f3689065f6f4258ace212c952c0f3
sudarshanapanditha <sudarshana.scholar@gmail.com>
2021-02-05 21:23:40 +0530

    Add UserServiceImplTest class
    Add UserServiceImplTest class, which contains all the tests related to
    UserServiceImpl and add the testSignInUser method which tests the
    correctness of signInUser method of UserServiceImpl

---
+++ Test/com/hcl/user/service/UserServiceImplTest.java
@@ -0,0 +1,34 @@
+package com.hcl.user.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.hcl.user.dao.UserDAO;
+import com.hcl.user.dao.UserDAOImpl;
+import com.hcl.user.domain.User;
+
+class UserServiceImplTest {
+	
+	private UserDAO userDAO;
+	private UserService userService;
+	
+	@BeforeEach
+	void initDAO() {
+		userDAO = new UserDAOImpl();
+		userService = new UserServiceImpl();
+	}
+
+	@Test
+	void testSignInUser() {
+		String username = "thomas123";
+		String password = "th123er";
+		User user = new User(username, "thomas", "hoover", "t.h@gmail.com", password);
+		userDAO.insertUser(user);
+		
+		assertTrue(userService.signInUser(username, password));
+		
+	}
+
+}
