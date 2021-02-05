e05efca4a18bb72f41495476974e843966daf573
sudarshanapanditha <sudarshana.scholar@gmail.com>
2021-02-05 21:05:57 +0530

    Minor changes

--- Test/com/hcl/user/dao/UserDAOImplTest.java
+++ Test/com/hcl/user/dao/UserDAOImplTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import com.hcl.seller.dao.AuctionItemDAOImpl;
 import com.hcl.user.domain.User;
 
 class UserDAOImplTest {
