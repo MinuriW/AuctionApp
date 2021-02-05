a11ea52a31511c1f3e5c197e8085e24281efa58e
sudarshanapanditha <sudarshana.scholar@gmail.com>
2021-02-05 10:06:19 +0530

    Add AuctionItemDAOImplTest class
    Add AuctionItemDAOImplTest class and add unit test to test
    insertAuctionItem method of AuctionItemDAO
    
    The AuctionItemDAOImplTest contains all the unit tests on
    AuctionItemDAO

---
+++ test/com/hcl/seller/dao/AuctionItemDAOImplTest.java
@@ -0,0 +1,38 @@
+package com.hcl.seller.dao;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.hcl.seller.domain.AuctionItem;
+
+class AuctionItemDAOImplTest {
+	
+	private AuctionItemDAO dao = null;
+	private AuctionItem auctionItem = null; 
+	
+	@BeforeEach
+	void initDAO() {
+		dao = new AuctionItemDAOImpl();
+		
+	}
+
+	@Test
+	void testSubmitAuctionItem() {
+		String title = "auction item";
+		String condition = "condition";
+		Timestamp startDate = new Timestamp(new Date().getTime());
+		Timestamp endDate = new Timestamp(new Date().getTime() +  10000);
+		Double startingPrice = 0.1;
+		String photoURL = "/path/to/photo";
+		auctionItem = new AuctionItem(title, condition, startDate, endDate, startingPrice, photoURL);
+		
+		assertTrue(dao.insertAuctionItem(auctionItem));
+		
+	}
+
+}
