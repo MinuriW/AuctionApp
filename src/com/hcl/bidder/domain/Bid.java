809031bd9feb5074fc4dfb161cd9fcf7e1e8a133
sudarshanapanditha <sudarshana.scholar@gmail.com>
2021-02-07 17:58:05 +0530

    Add new parameterized contstructor

--- src/com/hcl/bidder/domain/Bid.java
+++ src/com/hcl/bidder/domain/Bid.java
@@ -15,23 +15,29 @@ import com.hcl.seller.domain.AuctionItem;
 import com.hcl.user.domain.User;
 
 @Entity
-@Table(name="bid")
+@Table(name = "bid")
 public class Bid {
 	@Id
-	@SequenceGenerator(name="bid_seq", sequenceName="bid_seq", allocationSize=1)
-	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="bid_seq")
+	@SequenceGenerator(name = "bid_seq", sequenceName = "bid_seq", allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "bid_seq")
 	@Column
 	private int id;
-	@OneToOne(fetch=FetchType.EAGER)
-	@JoinColumn(name="auction_item_id")
+	@OneToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "auction_item_id")
 	private AuctionItem auctionItem;
-	@OneToOne(fetch=FetchType.EAGER)
-	@JoinColumn(name="auction_user_id")
+	@OneToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "auction_user_id")
 	private User user;
 
 	public Bid() {
 		super();
 		// TODO Auto-generated constructor stub
+	}
+
+	public Bid(AuctionItem auctionItem, User user) {
+		super();
+		this.auctionItem = auctionItem;
+		this.user = user;
 	}
 
 	public Bid(int id, AuctionItem auctionItem, User user) {
