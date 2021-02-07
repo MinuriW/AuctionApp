65b28f3609f2a45d4b1f07bbc297ca1539c0796a
sudarshanapanditha <sudarshana.scholar@gmail.com>
2021-02-07 19:36:06 +0530

    Add BidDAOTest class
    Add BidDAOTest class, which contains unit tests related to BidDAOImpl
    class, and add testInsertBid method, which tests insertBid method of
    BidDAOImpl class

---
+++ Test/com/hcl/bidder/dao/BidDAOTest.java
@@ -0,0 +1,65 @@
+package com.hcl.bidder.dao;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.hcl.bidder.domain.Bid;
+import com.hcl.seller.dao.AuctionItemDAO;
+import com.hcl.seller.dao.AuctionItemDAOImpl;
+import com.hcl.seller.domain.AuctionItem;
+import com.hcl.user.dao.UserDAO;
+import com.hcl.user.dao.UserDAOImpl;
+import com.hcl.user.domain.User;
+
+class BidDAOTest {
+	
+	private UserDAO userDAO;
+	private AuctionItemDAO auctionItemDAO;
+	private BidDAO bidDAO = null;
+	
+	private User user = null;
+	
+	private AuctionItem auctionItem = null;
+	
+	@BeforeEach
+	void initTest() {
+		userDAO = new UserDAOImpl();
+		auctionItemDAO = new AuctionItemDAOImpl();
+		bidDAO = new BidDAOImpl();
+		
+		if(user == null) {
+			user = new User("jax1", "jax", "teller", "jax1.soa.com", "jax123");
+			userDAO.insertUser(user);
+			user = userDAO.getUserByUsername("jax1");
+		}
+		
+		if(auctionItem == null) {
+			auctionItem = new AuctionItem(
+					"Awesome Ring", 
+					"Old" , 
+					"Super cool Viking ring for sale",
+					new Timestamp(new Date().getTime()), 
+					new Timestamp(new Date().getTime() + 1000 * 60 * 60 * 24 * 3),
+					15000.0, "photos/url/path/to/photo", false, user);
+			
+			auctionItemDAO.insertAuctionItem(auctionItem);
+			auctionItem = auctionItemDAO.getAllSubmittedItems(user).get(0);
+		}
+		
+	}
+	
+	@Test
+	void testInsertBid() {
+		Bid bid = new  Bid(auctionItem, user, 15001.0);
+		
+		Boolean isInserted = bidDAO.insertBid(bid);
+		
+		assertTrue(isInserted);
+	}
+
+}
