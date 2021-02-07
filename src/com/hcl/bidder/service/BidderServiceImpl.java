eb8611643e7bb8c48fada15e5c3693f5098d5d06
sudarshanapanditha <sudarshana.scholar@gmail.com>
2021-02-07 19:35:15 +0530

    Implement placeBid method

--- src/com/hcl/bidder/service/BidderServiceImpl.java
+++ src/com/hcl/bidder/service/BidderServiceImpl.java
@@ -1,13 +1,24 @@
 package com.hcl.bidder.service;
 
+import com.hcl.bidder.dao.BidDAO;
+import com.hcl.bidder.dao.BidDAOImpl;
+import com.hcl.bidder.domain.Bid;
 import com.hcl.seller.domain.AuctionItem;
 import com.hcl.user.domain.User;
 
 public class BidderServiceImpl implements BidderService {
+	
+	private BidDAO bidDAO;
+
+	public BidderServiceImpl() {
+		super();
+		bidDAO = new BidDAOImpl();
+	}
 
 	@Override
-	public Boolean placeBid(User user, AuctionItem auctionItem, double bid) {
-		return null;
+	public Boolean placeBid(User user, AuctionItem auctionItem, double amount) {
+		Bid bid = new Bid(auctionItem, user, amount);
+		return bidDAO.insertBid(bid);
 	}
 
 }
