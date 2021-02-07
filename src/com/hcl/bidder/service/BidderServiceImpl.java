c27f8d5c8c83fcd5c030aeb42f1aa38fe97a6073
sudarshanapanditha <sudarshana.scholar@gmail.com>
2021-02-07 17:31:27 +0530

    Add BidderServiceImpl class

---
+++ src/com/hcl/bidder/service/BidderServiceImpl.java
@@ -0,0 +1,13 @@
+package com.hcl.bidder.service;
+
+import com.hcl.seller.domain.AuctionItem;
+import com.hcl.user.domain.User;
+
+public class BidderServiceImpl implements BidderService {
+
+	@Override
+	public Boolean placeBid(User user, AuctionItem auctionItem, double bid) {
+		return null;
+	}
+
+}
