0550b8c62e712c524f2a5afac95b0b80dddc40c8
sudarshanapanditha <sudarshana.scholar@gmail.com>
2021-02-07 19:34:26 +0530

    Implement insertBid method

--- src/com/hcl/bidder/dao/BidDAOImpl.java
+++ src/com/hcl/bidder/dao/BidDAOImpl.java
@@ -1,12 +1,32 @@
 package com.hcl.bidder.dao;
 
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
 import com.hcl.bidder.domain.Bid;
+import com.hcl.util.HibernateUtil;
 
 public class BidDAOImpl implements BidDAO {
 
 	@Override
 	public Boolean insertBid(Bid bid) {
-		return null;
+		SessionFactory sf = HibernateUtil.getSessionFactory();
+		
+		Session session = sf.openSession();
+		session.beginTransaction();
+		
+		Boolean isInserted = true;
+		
+		try {
+			session.save(bid);
+		} catch (HibernateException e) {
+			isInserted = false;
+		}
+		
+		session.getTransaction().commit();
+		
+		return isInserted;
 	}
 
 }
