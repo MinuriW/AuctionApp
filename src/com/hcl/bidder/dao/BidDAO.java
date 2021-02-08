6a9b85210b94a16ae2a53485a1286f4bd65b6c91
sudarshanapanditha <sudarshana.scholar@gmail.com>
2021-02-08 10:30:37 +0530

    Implement insertBid method

--- src/com/hcl/bidder/dao/BidDAO.java
+++ src/com/hcl/bidder/dao/BidDAO.java
@@ -3,5 +3,9 @@ package com.hcl.bidder.dao;
 import com.hcl.bidder.domain.Bid;
 
 public interface BidDAO {
-	public Boolean insertBid(Bid bid);
+	public Bid insertBid(Bid bid);
+
+	public Boolean deleteBid(Bid bid);
+	
+	public Bid getBidById(int id);
 }
--- src/com/hcl/bidder/dao/BidDAOImpl.java
+++ src/com/hcl/bidder/dao/BidDAOImpl.java
@@ -10,23 +10,62 @@ import com.hcl.util.HibernateUtil;
 public class BidDAOImpl implements BidDAO {
 
 	@Override
-	public Boolean insertBid(Bid bid) {
+	public Bid insertBid(Bid bid) {
 		SessionFactory sf = HibernateUtil.getSessionFactory();
-		
+
 		Session session = sf.openSession();
 		session.beginTransaction();
-		
+
 		Boolean isInserted = true;
-		
+
 		try {
 			session.save(bid);
 		} catch (HibernateException e) {
 			isInserted = false;
 		}
-		
+
 		session.getTransaction().commit();
-		
-		return isInserted;
+
+		return isInserted ? bid : null;
+	}
+
+	@Override
+	public Boolean deleteBid(Bid bid) {
+		SessionFactory sf = HibernateUtil.getSessionFactory();
+
+		Session session = sf.openSession();
+		session.beginTransaction();
+
+		Boolean isDeleted = true;
+
+		try {
+			session.delete(bid);
+		} catch (HibernateException e) {
+			isDeleted = false;
+		}
+
+		session.getTransaction().commit();
+
+		return isDeleted;
+	}
+
+	@Override
+	public Bid getBidById(int id) {
+		SessionFactory sf = HibernateUtil.getSessionFactory();
+
+		Session session = sf.openSession();
+		session.beginTransaction();
+
+		Bid bid = null;
+
+		try {
+			bid = (Bid) session.get(Bid.class, new Integer(id));
+		} catch (HibernateException e) {
+		}
+
+		session.getTransaction().commit();
+
+		return bid;
 	}
 
 }
